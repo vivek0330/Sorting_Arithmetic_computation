3a5e778ccf13f788d2312f90abaa3bfe49cd64af
Vivek varshney <Vivek.iitd93@gmail.com>
2021-06-14 17:20:05 +0530

    Solve Airthmetic operartion

--- Sorting_Airthmetic.sh
+++ Sorting_Airthmetic.sh
@@ -2,10 +2,47 @@
 
 echo "Welcome to Arithmetic Computation"
 
-#TAKE USER INPUT 
-read -p "Enter the first value: " number1
-read -p "Enter the second value: " number2
-read -p "Enter the third value: " number3
+#TAKE USER INPUT
+read -p "Enter the first value a is: " number1
+read -p "Enter the second value b is: " number2
+read -p "Enter the third value c is: " number3
 
-#DISPLAY THE USER INPUTS
-echo $number1 $number2 $number3
+#Airthmetic operation
+output=`awk 'BEGIN{printf("%0.2f" , ('$number1+$number2*$number3'))}'`
+
+#Dislay the result
+echo $output
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+^G Help        ^O Write Out   ^W Where Is    ^K Cut         ^T Execute     ^C Location
+^X Exit        ^R Read File   ^\ Replace     ^U Paste       ^J Justify     ^_ Go To Line
+') )}'`
