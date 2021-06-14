a59823a4936351d08eb7ed59dfeadb7dfface6de
official_vivek <62185311+vivek0330@users.noreply.github.com>
2021-06-14 19:12:08 +0530

    Update Sorting_Airthmetic.sh

--- Sorting_Airthmetic.sh
+++ Sorting_Airthmetic.sh
@@ -12,37 +12,3 @@ output=`awk 'BEGIN{printf("%0.2f" , ('$number3+$number1/$number2'))}'`
 
 #Dislay the result
 echo $output
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-^G Help        ^O Write Out   ^W Where Is    ^K Cut         ^T Execute     ^C Location
-^X Exit        ^R Read File   ^\ Replace     ^U Paste       ^J Justify     ^_ Go To Line
-') )}'`
