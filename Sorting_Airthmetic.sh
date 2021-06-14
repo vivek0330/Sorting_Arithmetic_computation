07b3a78b7a233300ce99d2105361ce7e98b6af5d
Vivek varshney <Vivek.iitd93@gmail.com>
2021-06-14 17:27:25 +0530

    Solve Airthmetic operartion

--- Sorting_Airthmetic.sh
+++ Sorting_Airthmetic.sh
@@ -8,7 +8,7 @@ read -p "Enter the second value b is: " number2
 read -p "Enter the third value c is: " number3
 
 #Airthmetic operation
-output=`awk 'BEGIN{printf("%0.2f" , ('$number1+$number2*$number3'))}'`
+output=`awk 'BEGIN{printf("%0.2f" , ('$number1*$number2+$number3'))}'`
 
 #Dislay the result
 echo $output
