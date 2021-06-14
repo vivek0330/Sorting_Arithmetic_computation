ee038681b0d6d9eb171050e2dffbae9185551e24
Vivek varshney <Vivek.iitd93@gmail.com>
2021-06-14 17:46:04 +0530

    Store output in dictionary

--- Sorting_Airthmetic.sh
+++ Sorting_Airthmetic.sh
@@ -8,41 +8,19 @@ read -p "Enter the second value b is: " number2
 read -p "Enter the third value c is: " number3
 
 #Airthmetic operation
-output=`awk 'BEGIN{printf("%0.2f" , ('$number1%$number2+$number3'))}'`
+output1=`awk 'BEGIN{printf("%0.2f" , ('$number1+$number2*$number3'))}'`
+output2=`awk 'BEGIN{printf("%0.2f" , ('$number1*$number2+$number3'))}'`
+output3=`awk 'BEGIN{printf("%0.2f" , ('$number3+$number1/$number2'))}'`
+output4=`awk 'BEGIN{printf("%0.2f" , ('$number1%$number2+$number3'))}'`
 
-#Dislay the result
-echo $output
+#STORE THE ANSWER IN DICTONARY
+declare -A answerDictionary
 
+answerDictionary[output1]=$output1
+answerDictionary[output2]=$output2
+answerDictionary[output3]=$output3
+answerDictionary[output4]=$output4
 
+#Dislay the result in the dictionary format
+echo ${answerDictionary[@]}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-^G Help        ^O Write Out   ^W Where Is    ^K Cut         ^T Execute     ^C Location
-^X Exit        ^R Read File   ^\ Replace     ^U Paste       ^J Justify     ^_ Go To Line
-') )}'`
