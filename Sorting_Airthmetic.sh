584e1216bc29dc6d20b8d113010a494bdbff898e
Vivek varshney <Vivek.iitd93@gmail.com>
2021-06-14 17:04:14 +0530

    take three input from user

--- Sorting_Airthmetic.sh
+++ Sorting_Airthmetic.sh
@@ -1,3 +1,11 @@
 #!/bin/bash -x
 
 echo "Welcome to Arithmetic Computation"
+
+#TAKE USER INPUT 
+read -p "Enter the first value: " number1
+read -p "Enter the second value: " number2
+read -p "Enter the third value: " number3
+
+#DISPLAY THE USER INPUTS
+echo $number1 $number2 $number3
